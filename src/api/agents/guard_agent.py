65268330b9ec3b90976478052aefc19a61378453
gargalloeric <erickmontes555@gmail.com>
2025-01-12 13:53:46 +0100

    Tweak guard agent system prompt

--- src/api/agents/guard_agent.py
+++ src/api/agents/guard_agent.py
@@ -13,11 +13,12 @@ class GuardAgent(AgentInterface):
     def get_response(self, messages: List[Dict[str, Any]]) -> Dict[str, Any]:
         system_prompt = """
         You are a helpful AI assistant for a small restaurant application which serves food.
-        Your task is to determine whether the user is asking something relevant to the restaurant or not.
+        Your task is to determine whether the user is asking something relevant to the restaurant or not. In order to classify correctly the user request, you have to take into account the last message from the user.
+        
         The user is allowed to:
         1. Ask questions about the restaurant, like menu items and retaurant related questions.
         2. Ask questions about menu items, they can ask for the description or ingredients of the dishes.
-        3. Ask questions about who's your creator or who are you.
+        3. Ask questions about you, like who are you, who's your creator or which are your capabilities and purpose related to the restaurant.
         
         The user is NOT allowed to:
         1. Ask questions about anything else other than our restaurant.
@@ -25,8 +26,8 @@ class GuardAgent(AgentInterface):
 
         Your output should be in a structured json format like the following. Your are not allowed to output anything else than the structured json.
         {
-            "chain_of_thought": Go over each of the points above and make see if the message lies under this point or not. Then you write some of your thoughts about what points is this input relevant to. 
-            "allowed": `True` or `False`. Pick only one of those.
+            "chain_of_thought": Go over each of the points above and make see if the message lies under this point or not. Write in a list form all your thoughts about the user request. Your not allowd to write anithing else than your thoughts in a string format. 
+            "allowed": `true` or `false`. Pick only one of those.
             "message": Leave this property empty if it's allowed, otherwise write the reason why it's not allowed.
         }
         """
@@ -38,5 +39,6 @@ class GuardAgent(AgentInterface):
             self.model_name,
             input_messages
         )
+
         output = json.loads(response["message"]["content"])
         return output
